73a765d3a39dd2057f981d4265ae30054de84013
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-10-02 10:05:27 +0000

    [updating]コメント非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(comment_params)
-    comment.book_id = @book.id
-    comment.save
+    @book_comment = current_user.book_comments.new(comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+  
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    @comment = BookComment.find(params[:id])
-    @comment.destroy
+    @book_comment = BookComment.find(params[:id])
+    @book_comment.destroy
   end
 
 
